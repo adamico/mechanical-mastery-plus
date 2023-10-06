d7e66142e2a14db9c0880df23b50ad5e2b460505
Andrea D'Amico <andrea.damico@gmail.com>
2023-10-06 12:46:54 +0200

    change cube1 and cube2 emc, new steel dust recipe

--- kubejs/server_scripts/mods/create.js
+++ kubejs/server_scripts/mods/create.js
@@ -1,7 +1,10 @@
 ServerEvents.recipes(event => {
   event.remove({id: 'create:milling/sandstone'});
 	event.recipes.createMilling([Item.of('minecraft:sand'), Item.of('thermal:sulfur').withChance(0.8)], 'minecraft:sandstone');
+
   event.recipes.createHaunting('minecraft:egg', 'create:dough');
+
+	event.createMixing('mekanism:dust_steel', [Item.of('minecraft:iron_ingot', 3), 'minecraft:coal', 'minecraft:charcoal']).processingTime(60);
 	event.remove({id: 'createchromaticreturn:motor_recipe'});
 	event.remove({id: 'createchromaticreturn:cf_to_gp'});
 	event.remove({id: 'createchromaticreturn:gp_to_bp'});
