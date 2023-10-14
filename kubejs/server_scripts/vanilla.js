ff7a194301fe95af4ada828495c751359b22181a
adamico <andrea.damico@gmail.com>
2023-10-14 19:27:28 +0200

    added recipe: 4 chests from 8 logs fixes #63

--- kubejs/server_scripts/vanilla.js
+++ kubejs/server_scripts/vanilla.js
@@ -1,4 +1,9 @@
 ServerEvents.recipes(event => {
 	event.shapeless(Item.of('minecraft:andesite', 2), [Item.of('minecraft:cobblestone', 2), Item.of('projecte:high_covalence_dust', 2)]);
 	event.shapeless(Item.of('minecraft:andesite', 8), [Item.of('minecraft:cobblestone', 8), Item.of('minecraft:quartz', 1)]);
-})
+	event.shaped(Item.of('minecraft:chest', 4), [
+		'LLL',
+		'L L',
+		'LLL'
+		], { L: '#minecraft:logs' });
+	});
