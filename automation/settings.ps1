c3ac7e807c04074f7e7e3e1edb1dd6689d2c8876
adamico <andrea.damico@gmail.com>
2023-11-10 18:11:18 +0100

    1.0.9f

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -30,12 +30,12 @@ $MODPACK_NAME = "mechanical-mastery-plus"
 $CLIENT_NAME = "Mechanical Mastery Plus"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "r1.0.9"
+$MODPACK_VERSION = "r1.0.9f"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
 # Should be "$null" if this is the first release
-$LAST_MODPACK_VERSION = "r1.0.8f"
+$LAST_MODPACK_VERSION = "r1.0.9"
 
 # Which modloader the modpack uses
 # Can be "forge" or "fabric"
---
+++ changelogs/changelog_mods_r1.0.9f.md
@@ -0,0 +1,6 @@
+## mechanical-mastery-plus - r1.0.9 -> r1.0.9f
+
+### Changed
+
+  * [adminshop-1.19.2-1.14.jar](https://www.curseforge.com/minecraft/mc-mods/adminshop/files/4802884) -> [adminshop-1.19.2-1.15.1.jar](https://www.curseforge.com/minecraft/mc-mods/adminshop/files/4856776)
+
