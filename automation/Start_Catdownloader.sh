b80f81dea6834f47b49061675503932884c2ff73
Andrea D'Amico <andrea.damico@gmail.com>
2023-11-21 15:33:19 +0100

    Add files via upload

---
+++ automation/Start_Catdownloader.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This script is used to configure and run CatDownloader!
+# You can add few additional arguments to change the behaviour of the app.
+# Check out the github repo of this app for more details https://github.com/Kanzaji/Cat-Downloader-Legacy
+
+# You can configure the command here:
+java -jar Cat-Downloader-Legacy-2.1.3.jar
