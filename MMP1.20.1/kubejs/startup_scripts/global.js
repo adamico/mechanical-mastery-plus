9d6107069ea785eefe7bb58967243732e2700c5f
Andrea D'Amico <andrea.damico@gmail.com>
2023-11-23 10:51:26 +0100

    1.20.1 test

--- MMP1.20.1/kubejs/startup_scripts/global.js
+++
@@ -1,2 +0,0 @@
-// priority: 900
-global.all_metals = ['iron', 'gold', 'osmium', 'copper', 'tin', 'lead', 'uranium', 'silver', 'nickel', 'zinc'];
--- MMP1.20.1/kubejs/startup_scripts/item_modification.js
+++
@@ -1,8 +0,0 @@
-ItemEvents.modification(event => {
-  event.modify('projectexpansion:infinite_steak', item => {
-      item.setFoodProperties(food => {
-          food.hunger(5);
-          food.saturation(5);
-      });
-  });
-});
