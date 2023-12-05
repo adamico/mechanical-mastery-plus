16a7f1b00c0cb51cb50539d834677af3bc1306f5
Andrea D'Amico <andrea.damico@gmail.com>
2023-12-05 16:30:47 +0100

    [Feature] Add chest recipe from Menril planks #131
    and 4 from 8 menril logs

--- MMP1.19.2/kubejs/server_scripts/recipes/vanilla.js
+++ MMP1.19.2/kubejs/server_scripts/recipes/vanilla.js
@@ -9,4 +9,25 @@ ServerEvents.recipes(event => {
   }
 
   recipeCycle(['wheat_seeds', 'beetroot_seeds', 'carrot', 'potato', 'melon_seeds', 'pumpkin_seeds', 'cocoa_beans', 'sweet_berries'])
+
+  event.shaped(
+    'minecraft:chest',
+    [
+      'PPP',
+      'P P',
+      'PPP',
+    ],
+    { P: 'integrateddynamics:menril_planks' },
+  ).id('kubejs:chest_from_menril_planks')
+
+  event.shaped(
+    Item.of('minecraft:chest', 4),
+    [
+      'LLL',
+      'L L',
+      'LLL',
+    ],
+    { L: '#integrateddynamics:menril_logs' },
+  ).id('kubejs:4_chests_from_menril_logs')
+
 })
