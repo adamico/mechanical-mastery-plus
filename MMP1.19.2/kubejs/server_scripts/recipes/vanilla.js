8dd50ebcc910b1d93823b95d6c7f6c9cf7013e85
Andrea D'Amico <andrea.damico@gmail.com>
2023-12-05 16:03:49 +0100

    [bug] Carrot and Beetroots are not obtainable before having access to emeralds #130
    finally decided to implement a circular recipe from wheat to sweet berries

--- MMP1.19.2/kubejs/server_scripts/recipes/vanilla.js
+++ MMP1.19.2/kubejs/server_scripts/recipes/vanilla.js
@@ -1,2 +1,12 @@
 ServerEvents.recipes(event => {
-});
+  let recipeCycle = (itemIds) => {
+    itemIds.forEach((element, index, array) => {
+      let input = element
+      let output = (element === array[array.length - 1]) ? array[0] : array[index + 1]
+
+      event.shapeless(output, [input])
+    })
+  }
+
+  recipeCycle(['wheat_seeds', 'beetroot_seeds', 'carrot', 'potato', 'melon_seeds', 'pumpkin_seeds', 'cocoa_beans', 'sweet_berries'])
+})
