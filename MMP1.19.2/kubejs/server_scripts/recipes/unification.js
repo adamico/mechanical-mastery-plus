2d49d523c066d15cabad1bbde212797bb404fff4
Andrea D'Amico <andrea.damico@gmail.com>
2023-12-08 18:49:29 +0100

    [Feature] Unify uranium and yellorite
    fixes #138

--- MMP1.19.2/kubejs/server_scripts/recipes/unification.js
+++ MMP1.19.2/kubejs/server_scripts/recipes/unification.js
@@ -1,4 +1,46 @@
 ServerEvents.recipes(event => {
+  //functions
+  let bigreactors_fluidizersolidmixing = (ing1, ing1Count, ing2, ing2Count, result, resultAmount) => {
+    event.custom({
+      "type": "bigreactors:fluidizersolidmixing",
+      "ingredient1": {
+        "ingredient": {
+          "item": ing1
+        },
+        "count": ing1Count
+      },
+      "ingredient2": {
+        "ingredient": {
+          "item": ing2
+        },
+        "count": ing2Count
+      },
+      "result": {
+        "fluid": result,
+        "count": resultAmount
+      }
+    })
+    event.custom({
+      "type": "bigreactors:fluidizersolidmixing",
+      "ingredient1": {
+        "ingredient": {
+          "item": ing2
+        },
+        "count": ing2Count
+      },
+      "ingredient2": {
+        "ingredient": {
+          "item": ing1
+        },
+        "count": ing1Count
+      },
+      "result": {
+        "fluid": result,
+        "count": resultAmount
+      }
+    })
+  }
+
   // remove thermal:flax_block because apart storage purposes is not a functional block
   event.remove([{ output: 'thermal:flax_block' }, { input: 'thermal:flax_block' }])
 
@@ -11,4 +53,13 @@ ServerEvents.recipes(event => {
   // obtain string from any #forge:crops/flax
   event.remove({ id: 'supplementaries:strings' })
   event.shapeless('minecraft:string', '#forge:crops/flax').id('string_from_flax')
+
+  event.remove({ id: 'bigreactors:misc/book/erguide' })
+
+  event.remove({ id: 'bigreactors:fluidizer/solidmixing/verderium_2' })
+  event.remove({ id: 'bigreactors:fluidizer/solidmixing/verderium_1' })
+  event.remove({ id: 'bigreactors:fluidizer/solidmixing/verderium9_1' })
+  event.remove({ id: 'bigreactors:fluidizer/solidmixing/verderium9_2' })
+  bigreactors_fluidizersolidmixing('mekanism:ingot_uranium', 2, 'bigreactors:blutonium_ingot', 1, 'bigreactors:verderium', 2000)
+  bigreactors_fluidizersolidmixing('mekanism:block_uranium', 2, 'bigreactors:blutonium_block', 1, 'bigreactors:verderium', 2000)
 })
